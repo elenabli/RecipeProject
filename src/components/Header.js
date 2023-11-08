3bf70617b683ec7ee438b7fd244afe4a1099e8de
elenabli <elena.blinova@metropolia.fi>
2023-11-08 22:57:45 +0200

    added components header, navbar, footer

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import NavBar from "./NavBar";
+
+function Header() {
+    return <div>
+        <div className="header">
+            <h1>mealMIX</h1>
+            <NavBar />
+        </div>
+    </div>;
+  }
+  
+  export default Header;
---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+const NavBar = () => {
+    return (
+      <div>
+        <nav className="navbar">
+            <ul className="nav-menu">
+                <li className="nav-item">
+                <a href="#">Home</a>
+                </li>
+                <li className="nav-item">
+                <a href="#">Recipes</a>
+                </li>
+                <li className="nav-item">
+                <a href="#">About</a>
+                </li>
+            </ul>
+        </nav>
+      </div>
+    );
+  };
+  
+  export default NavBar;
