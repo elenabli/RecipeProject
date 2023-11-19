2a36efee346883d06292bb85822bbbc46bc9f746
elenabli <111984410+elenabli@users.noreply.github.com>
2023-11-19 17:12:40 +0200

    Revert "Add components: Recipe, Recipes"

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Footer from './components/Footer';
 import Header from "./components/Header";
 import "./App.css";
 import Hero from "./components/Hero";
-import Recipes from "./components/Recipes";
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
   
         <Header />
         <Hero />
-        <Recipes />
         <Footer />
       </div>
    
