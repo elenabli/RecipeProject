732e5df8cc9a9fa9489311e0a27e3f9dcbe6122c
elenabli <elena.blinova@metropolia.fi>
2023-12-03 21:03:16 +0200

    Change recipes render logic, split to 2 component

---
+++ frontend/src/components/Recipes.js
@@ -0,0 +1,34 @@
+import { useContext } from 'react';
+import { RecipeContext } from '../context/RecipeContext'
+import Recipe from './Recipe'
+
+const Recipes = () => {
+    const {selectedRecipes} = useContext(RecipeContext);
+    const shoppingList = selectedRecipes ? selectedRecipes.flatMap((recipe) => recipe.ingredients) : [];
+
+  return (
+    <div>
+        {selectedRecipes ? 
+        (<><h2>Recipes</h2>
+        <div className="recipe-container">
+        {selectedRecipes && selectedRecipes.map((recipe) => (
+          <Recipe key={recipe._id} {...recipe} />
+        ))}
+      </div>
+      <h2>Shopping list</h2>
+        <div>
+        <ul>
+          {selectedRecipes && shoppingList.map((ingredient, index) => (
+            <li key={index}>{ingredient.name}</li>
+          ))}
+        </ul>
+        </div>
+      </>) : (
+        <h2>Click the button to get recipes!</h2>
+      
+      ) }
+    </div>
+  )
+}
+
+export default Recipes
